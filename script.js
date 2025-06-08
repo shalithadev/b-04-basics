e0428d6961f870011ddc74d64208b2c80e12cd11
hashaaan <shalithadev@gmail.com>
2025-06-08 20:52:16 +0530

    07/06/2025: Session 02 - B04

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // External JavaScript file
 
+// JavaScript Variable Types
 var userEmail = "shalithadev@gmail.com";
-const userName = "Hashan Shalitha";
-let userAge = 28;
+const userName = "Hashan Shalitha"; // String
+let userAge = 28; // Number
+const isAuthenticated = false; // Boolean
+const lastName = null;
+const firstName = undefined;
+const userData = {
+  firstName: "Hashan",
+  lastName: "Shalitha",
+  age: 29,
+};
+// Arrays
+const fruits = ["banana", "apple", "guava"]; // String Array
+const numbers = [1, 2, 3, 4]; // Number Array
+const users = [
+  { id: 1, name: "Hashan" },
+  { id: 2, name: "Shalitha" },
+  { id: 3, name: "Kavindu" },
+]; // Object Array
 
 // userName = "Shalitha Hashan"
 
@@ -16,4 +33,14 @@ comment
 
 userAge = 29;
 
-console.log("I'm second javascript file here!", {userEmail, userName, userAge});
+// console.log("I'm second javascript file here!", {userEmail, userName, userAge});
+
+// console.log(userEmail, typeof userEmail);
+// console.log(userAge, typeof userAge);
+// console.log("value:", isAuthenticated, "type:", typeof isAuthenticated);
+// console.log("value:", lastName, "type:", typeof lastName);
+// console.log("value:", firstName, "type:", typeof firstName);
+// console.log("value:", userData, "type:", typeof userData);
+// console.log("value:", fruits, "type:", typeof fruits);
+// console.log("value:", numbers, "type:", typeof numbers);
+console.log("value:", users, "type:", typeof users);
